d5e6b2238997e5f5517dd2b93a0291dd8feb4e24
ccuuimi <daisyingdong@gmail.com>
2020-06-18 22:27:20 -0400

    Initial Commit

---
+++ Art YourselfTests/Art_YourselfTests.swift
@@ -0,0 +1,34 @@
+//
+//  Art_YourselfTests.swift
+//  Art YourselfTests
+//
+//  Created by Ying Dong on 6/18/20.
+//  Copyright © 2020 whoisyingdong. All rights reserved.
+//
+
+import XCTest
+@testable import Art_Yourself
+
+class Art_YourselfTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
